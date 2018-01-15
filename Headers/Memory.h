7200aecec8297aca08afcf2536cb17650258f12f
RyanFitzpatrick <rfitzp02@uoguelph.ca>
2018-01-15 14:35:22 -0500

    Added Memory.h

---
+++ Headers/Memory.h
@@ -0,0 +1,28 @@
+/* Memory.h: Defines various macros used for memory management */
+
+#ifndef _CH_MEMORY_
+#define _CH_MEMORY_
+
+/* Memory.h requires the malloc and free functions defined in stdlib.h */
+#ifndef _STDLIB_
+#define _STDLIB_
+#include <stdlib.h>
+#endif
+
+/* Allocates a new pointer using malloc */
+/* NOTE: If malloc fails this macro will jump to the FAIL label, so the calling code must have a FAIL label */
+/* Param1 ptr: The pointer to be allocated */
+/* Param2 size: The size of the memory to point at, clean up with DiscardMem */
+#define NewMem(ptr, size) if ((ptr = malloc(size)) == NULL) goto FAIL
+
+/* Resizes a pointer using realloc */
+/* NOTE: If realloc fails this macro will jump to the FAIL label, so the calling code must have a FAIL label */
+/* Param1 ptr: The pointer to be reallocated */
+/* Param2 size: The size of the memory to point at, clean up with DiscardMem */
+#define ResizeMem(ptr, size) { void * __ResizeMem__ = NULL; if ((__ResizeMem__ = realloc(ptr, size)) == NULL) goto FAIL; ptr = __ResizeMem__; }
+
+/* Releases a pointer using free */
+/* Param1 ptr: The pointer to be released */
+#define DiscardMem(ptr) free(ptr)
+
+#endif
--- Headers/TriTree.h
+++ Headers/TriTree.h
@@ -1,1 +1,17 @@
+#ifndef _TRITREE_
+#define _TRITREE_
+
+#define BOTTOM 0
+#define LEFT 1
+#define RIGHT 2
+
+typedef struct TriTree
+{
+    struct TriTree * sides[3];
+    struct TriTree * left;
+    struct TriTree * right;
+} TriTree;
+
 int hi();
+
+#endif
