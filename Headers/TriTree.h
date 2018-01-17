b7f4e853ae539dea24d05a2d79c091062f73477c
RyanFitzpatrick <rfitzp02@uoguelph.ca>
2018-01-17 12:39:11 -0500

    Added some comments

--- Headers/TriTree.h
+++ Headers/TriTree.h
@@ -1,22 +1,49 @@
+/* TriTree.h: Defines the TriTree type as well various function prototypes and macros to be used with this type */
+
 #ifndef _TRITREE_
 #define _TRITREE_
 
+/* A constant rerpresenting the index in a TriTree's sides array that corresponds to the bottom side */
 #define BOTTOM 0
+/* A constant rerpresenting the index in a TriTree's sides array that corresponds to the left side */
 #define LEFT 1
+/* A constant rerpresenting the index in a TriTree's sides array that corresponds to the right side */
 #define RIGHT 2
 
+/* The structure used to represent a binary triangle tree */
 typedef struct TriTree
 {
+    /* An array containing pointers to a triangle's 3 neighbours */
     struct TriTree * sides[3];
+    /* A triangle's left child */
     struct TriTree * left;
+    /* A triangle's right child */
     struct TriTree * right;
 } TriTree;
 
+/* Initalizes a new binary triangle tree */
+/* NOTE: It's recommended to use the BuildTriTree macro instead of calling this directly */
+/* Returns (TriTree *): A pointer to a newly allocated binary triangle tree, or NULL on error */
 TriTree * _BuildTriTree(void);
+
+/* Splits a binary triangle tree node into 2 new triangles */
+/* NOTE: It's recommended to use the SplitTriTree macro instead of calling this directly */
+/* Param1 (TriTree *): A pointer to the binary triangle tree node to be split */
+/* Returns (int): 1 if split was successful, 0 otherwise */
 int _SplitTriTree(TriTree *);
+
+/* Frees all memory used by a binary triangle tree */
+/* Param1 (TriTree *): A pointer to the binary triangle tree to be freed */
 void DiscardTriTree(TriTree *);
 
+/* Calls _BuildTriTree to initalize a binary triangle tree and handles any errors */
+/* NOTE: This macro jumps to the FAIL label on error, so the calling code must have a FAIL label */
+/* Param1 (TriTree *) tree: A pointer to the binary triangle tree to be initalized */
 #define BuildTriTree(tree) if ((tree = _BuildTriTree()) == NULL) goto FAIL
+
+/* Calls _SplitTriTree to split a binary triangle tree and handles any errors */
+/* NOTE: This macro jumps to the FAIL label on error, so the calling code must have a FAIL label */
+/* Param1 (TriTree *) tree: A pointer to the binary triangle tree to be split */
 #define SplitTriTree(tree) if (!_SplitTriTree(tree)) goto FAIL
 
 #endif
--- Source/TriTree.c
+++ Source/TriTree.c
@@ -1,12 +1,21 @@
+/* TriTree.c: Implements various functions defined in TriTree.h */
+
 #include "TriTree.h"
 #include "Memory.h"
 
+/* Force splits a binary triangle tree node into 2 triangles */
+/* Param1 (TriTree *): A pointer to the binary triangle tree node to be split */
+/* Returns (int): 1 if split was successful, 0 otherwise */
 static int Force(TriTree *);
 
+/* Initalizes a new binary triangle tree */
+/* NOTE: It's recommended to use the BuildTriTree macro instead of calling this directly */
+/* Returns (TriTree *): A pointer to a newly allocated binary triangle tree, or NULL on error */
 TriTree * _BuildTriTree(void)
 {
     TriTree * tree = NULL;
 
+    /* Allocate memory for the binary triangle tree and give it's fields default values */
     NewMem(tree, sizeof(TriTree));
     tree->sides[BOTTOM] = NULL;
     tree->sides[LEFT] = NULL;
@@ -20,16 +29,23 @@ TriTree * _BuildTriTree(void)
         return NULL;
 }
 
+/* Splits a binary triangle tree node into 2 new triangles */
+/* NOTE: It's recommended to use the SplitTriTree macro instead of calling this directly */
+/* Param (TriTree *) tree: A pointer to the binary triangle tree node to be split */
+/* Returns (int): 1 if split was successful, 0 otherwise */
 int _SplitTriTree(TriTree * tree)
 {
     if (tree->sides[BOTTOM])
     {
+        /* Determine if we need to recursively split the bottom triangle to avoid mesh cracks */
         if (tree->sides[BOTTOM]->sides[BOTTOM] != tree)
             _SplitTriTree(tree->sides[BOTTOM]);
 
+        /* Split the current node and it's bottom side neighbour */
         if (!Force(tree)) goto FAIL;
         if (!Force(tree->sides[BOTTOM])) goto FAIL;
 
+        /* Update the sides of the new triangles */
         tree->left->sides[RIGHT] = tree->sides[BOTTOM]->right;
         tree->right->sides[LEFT] = tree->sides[BOTTOM]->left;
         tree->sides[BOTTOM]->left->sides[RIGHT] = tree->right;
@@ -37,6 +53,7 @@ int _SplitTriTree(TriTree * tree)
     }
     else
     {
+        /* If the current node has no bottom neighbour then a basic forced split will suffice */
         if (!Force(tree)) goto FAIL;
         tree->left->sides[RIGHT] = NULL;
         tree->right->sides[LEFT] = NULL;
@@ -48,15 +65,22 @@ int _SplitTriTree(TriTree * tree)
         return 0;
 }
 
+/* Force splits a binary triangle tree node into 2 triangles */
+/* Param (TriTree *) tree: A pointer to the binary triangle tree node to be split */
+/* Returns (int): 1 if split was successful, 0 otherwise */
 static int Force(TriTree * tree)
 {
+    /* Initialize the 2 new triangles */
     BuildTriTree(tree->left);
     BuildTriTree(tree->right);
 
+    /* Update the new triangle sides */
     tree->left->sides[LEFT] = tree->right;
     tree->right->sides[RIGHT] = tree->left;
     tree->left->sides[BOTTOM] = tree->sides[LEFT];
+    tree->right->sides[BOTTOM] = tree->sides[RIGHT];
 
+    /* Update the node's left neighbour */
     if (tree->sides[LEFT])
     {
         if (tree->sides[LEFT]->sides[BOTTOM] == tree)
@@ -67,8 +91,7 @@ static int Force(TriTree * tree)
             tree->sides[LEFT]->sides[RIGHT] = tree->left;
     }
 
-    tree->right->sides[BOTTOM] = tree->sides[RIGHT];
-
+    /* Update the node's right neighbour */
     if (tree->sides[RIGHT])
     {
         if (tree->sides[RIGHT]->sides[BOTTOM] == tree)
@@ -85,12 +108,15 @@ static int Force(TriTree * tree)
         return 0;
 }
 
+/* Frees all memory used by a binary triangle tree */
+/* Param (TriTree *) tree: A pointer to the binary triangle tree to be freed */
 void DiscardTriTree(TriTree * tree)
 {
-    if (tree == NULL)
-        return;
-
-    DiscardTriTree(tree->left);
-    DiscardTriTree(tree->right);
-    DiscardMem(tree);
+    /* Recursively traverse the binary triangle tree and free all nodes */
+    if (tree)
+    {
+        DiscardTriTree(tree->left);
+        DiscardTriTree(tree->right);
+        DiscardMem(tree);
+    }
 }
